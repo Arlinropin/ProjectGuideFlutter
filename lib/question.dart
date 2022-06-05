97fd74ce4e6ab68a5dc012da7393c67dfe102eef
Arlin_Ropero <alroperoi@unal.edu.co>
2022-06-05 09:20:24 -0500

    Ajuste

--- lib/question.dart
+++ lib/question.dart
@@ -3,6 +3,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Question extends StatelessWidget {
+  final String
+      questionText; // 'final' es para decir que el valor no cambiara una vez se inicialice.
+  Question(this.questionText);
   @override
   Widget build(BuildContext context) {
     return Container();
